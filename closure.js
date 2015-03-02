839f0ba83720b2ce966ddccb6c747a8fe97a7cff
Teszko <b.iwaniuk@campus.tu-berlin.de>
2015-03-03 00:31:24 +0100

    more bla

--- closure.js
+++ closure.js
@@ -11,14 +11,17 @@ function getItem (n) {
 
 console.log(getItem(3)); // 0x000C (12)
 
+
 // Langsam, da bei jedem Aufruf item neu alloziert wird.
 // Kann mithilfe von Closures behoben werden.
 
-var getItemClosure = (function () { // Funktion deklariert als Expression
+
+var getItemClosure = (function () { // Anonyme Funktion umschließt Variablen
     var item = [0xAF01, 0x1011,
         0xAAA1, 0x000C, 0x1234, 0xA1C6];
 
-    return function (n) {
+    return function (n) { // die eigentliche Funktion, die an getItemClosure returnt wird.
+                          // Die Variable item bleibt im Scope von getItemClosure erhalten auch nach dem return.
         return item[n];
     };
 }());
