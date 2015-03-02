0dc0b59d46e7d2ce887a45531293d0b1ddac9485
Teszko <b.iwaniuk@campus.tu-berlin.de>
2015-03-03 00:55:14 +0100

    made it more compact

--- closure.js
+++ closure.js
@@ -1,5 +1,10 @@
 /**
  * JavaScript Closures
+ * getItem und getItemClosure gleichen sich im Verhalten. Der Unterschied liegt in der Performance.
+ * bei jedem Aufruf von getItem wird Speicher für die Array item alloziert.
+ * getItemClosure umgeht dieses Problem durch die Nutzung von Closures.
+ * Die Variablen, die von der anonymen Funktion umschlossen werden, sind auch nach dem return noch
+ * im Scope von getItemClosure gültig.
  */
 
 function getItem (n) {
@@ -9,21 +14,12 @@ function getItem (n) {
     return item[n];
 }
 
-console.log(getItem(3)); // 0x000C (12)
 
-
-// Langsam, da bei jedem Aufruf item neu alloziert wird.
-// Kann mithilfe von Closures behoben werden.
-
-
-var getItemClosure = (function () { // Anonyme Funktion umschließt Variablen
+var getItemClosure = (function () {
     var item = [0xAF01, 0x1011,
         0xAAA1, 0x000C, 0x1234, 0xA1C6];
 
-    return function (n) { // die eigentliche Funktion, die an getItemClosure returnt wird.
-                          // Die Variable item bleibt im Scope von getItemClosure erhalten auch nach dem return.
+    return function (n) {
         return item[n];
     };
 }());
-
-console.log(getItemClosure(3)); // 0x000C (12)
